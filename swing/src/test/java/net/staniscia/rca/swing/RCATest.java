e19d80c4bc36003959a585cb18777c77342326f5
Alessandro Staniscia <alessandro@staniscia.net>
2014-02-26 15:22:44 +0100

    enable HCI on travis-ci

--- swing/src/test/java/net/staniscia/rca/swing/RCATest.java
+++ swing/src/test/java/net/staniscia/rca/swing/RCATest.java
@@ -6,78 +6,57 @@ import net.staniscia.rca.swing.common.windows.DummyContent;
 import net.staniscia.rca.swing.common.windows.MemoryView;
 import org.junit.Test;
 
-import java.awt.*;
-
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
 public class RCATest {
 
     @Test
     public void testShow() {
-        try {
             RCA rca = new RCA();
             rca.show("Test");
             rca.getViewService().add(new MemoryView());
             rca.shutdownComponents();
-        } catch (HeadlessException e) {
-            assertTrue("No GUI AVAILABLE", true);
-        }
     }
 
 
     @Test
     public void testGetViewService() {
-        try {
+
             RCA rca = new RCA();
             rca.show("Test");
             assertNotNull(rca.getViewService());
             rca.shutdownComponents();
-        } catch (HeadlessException e) {
-            assertTrue("No GUI AVAILABLE", true);
-        }
     }
 
     @Test
     public void testGetContentService() {
-        try {
+
             RCA rca = new RCA();
             rca.show("Test");
             assertNotNull(rca.getContentService());
             rca.shutdownComponents();
-        } catch (HeadlessException e) {
-            assertTrue("No GUI AVAILABLE", true);
-        }
     }
 
     @Test
     public void testGetActionService() {
-        try {
             RCA rca = new RCA();
             rca.show("Test");
             assertNotNull(rca.getActionService());
             rca.shutdownComponents();
-        } catch (HeadlessException e) {
-            assertTrue("No GUI AVAILABLE", true);
-        }
     }
 
     @Test
     public void testGetStatusBarService() {
-        try {
             RCA rca = new RCA();
             rca.show("Test");
             assertNotNull(rca.getStatusBarService());
             rca.shutdownComponents();
-        } catch (HeadlessException e) {
-            assertTrue("No GUI AVAILABLE", true);
-        }
     }
 
 
     @Test
     public void testViewMULTIPLEaddEndRemove() {
-        try {
+
             RCA rca = new RCA();
             rca.show("Test");
             ViewServices viewM = rca.getViewService();
@@ -103,15 +82,12 @@ public class RCATest {
             }
 
             rca.shutdownComponents();
-        } catch (HeadlessException e) {
-            assertTrue("No GUI AVAILABLE", true);
-        }
     }
 
 
     @Test
     public void testContentMULTIPLEaddEndRemove() {
-        try {
+
             RCA rca = new RCA();
             rca.show("Test");
             ContentServices viewM = rca.getContentService();
@@ -137,9 +113,6 @@ public class RCATest {
             }
 
             rca.shutdownComponents();
-        } catch (HeadlessException e) {
-            assertTrue("No GUI AVAILABLE", true);
-        }
     }
 
 }
